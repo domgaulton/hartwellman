f79a5d1fa7d6a2df55497058bd77d61597304627
Dom Gaulton <d.gaulton@catchdigital.com>
2019-10-28 14:57:16 +0000

    Update favicon

--- src/App.js
+++ src/App.js
@@ -35,12 +35,14 @@ function App() {
   }
 
   const handleInputChange = newValue => {
-    if (newValue.length > 1) {
-      newValue.forEach(item => {
-        updateStateWithStats(item);
-      })
-    } else {
-      updateStateWithStats(newValue[0]);
+    if (newValue){
+      if (newValue.length > 1) {
+        newValue.forEach(item => {
+          updateStateWithStats(item);
+        })
+      } else {
+        updateStateWithStats(newValue[0]);
+      }
     }
   };
 
@@ -95,55 +97,55 @@ function App() {
         onChange={(e) => handleInputChange(e)}
       />
       <div style={{height: '400px'}}>
-      <ResponsiveRadar
-        data={superHeroList}
-        keys={getKeys()}
-        indexBy="stat"
-        maxValue={100}
-        margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-        curve="linearClosed"
-        borderWidth={2}
-        borderColor={{ from: 'color' }}
-        gridLevels={5}
-        gridShape="circular"
-        gridLabelOffset={36}
-        enableDots={true}
-        dotSize={10}
-        dotColor={{ theme: 'background' }}
-        dotBorderWidth={2}
-        dotBorderColor={{ from: 'color' }}
-        enableDotLabel={true}
-        dotLabel="value"
-        dotLabelYOffset={-12}
-        colors={{ scheme: 'nivo' }}
-        fillOpacity={0.1}
-        blendMode="multiply"
-        animate={true}
-        motionStiffness={90}
-        motionDamping={15}
-        isInteractive={true}
-        legends={[
-            {
-                anchor: 'top-left',
-                direction: 'column',
-                translateX: -50,
-                translateY: -40,
-                itemWidth: 80,
-                itemHeight: 20,
-                itemTextColor: '#999',
-                symbolSize: 12,
-                symbolShape: 'circle',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemTextColor: '#000'
-                        }
-                    }
-                ]
-            }
-        ]}
-      />
+        <ResponsiveRadar
+          data={superHeroList}
+          keys={getKeys()}
+          indexBy="stat"
+          maxValue={100}
+          margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+          curve="linearClosed"
+          borderWidth={2}
+          borderColor={{ from: 'color' }}
+          gridLevels={5}
+          gridShape="circular"
+          gridLabelOffset={36}
+          enableDots={true}
+          dotSize={10}
+          dotColor={{ theme: 'background' }}
+          dotBorderWidth={2}
+          dotBorderColor={{ from: 'color' }}
+          enableDotLabel={true}
+          dotLabel="value"
+          dotLabelYOffset={-12}
+          colors={{ scheme: 'nivo' }}
+          fillOpacity={0.1}
+          blendMode="multiply"
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+          isInteractive={true}
+          legends={[
+              {
+                  anchor: 'top-left',
+                  direction: 'column',
+                  translateX: -50,
+                  translateY: -40,
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemTextColor: '#999',
+                  symbolSize: 12,
+                  symbolShape: 'circle',
+                  effects: [
+                      {
+                          on: 'hover',
+                          style: {
+                              itemTextColor: '#000'
+                          }
+                      }
+                  ]
+              }
+          ]}
+        />
       </div>
     </div>
   );
